0cde54bc16cd48ef9dd09644c9db410a130c7ea4
Achinta <achintasiddhanta0304@gmail.com>
2026-05-15 02:20:07 +0530

    added fetch teams api

--- admin/src/lib/api.ts
+++ admin/src/lib/api.ts
@@ -1,8 +1,14 @@
 import axios from "axios"
-import type { UserDetails } from "@/components/ui/columns"
 
 export const fetchUserDetails = async () => {
-    const response: {data: UserDetails[]} = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/team-members`)
+    const response: { data: TeamMember[] } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/team-members`)
 
     return response.data
+}
+
+export const getAllTeams = async () => {
+    const response: { data: Team[] } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/teams`)
+
+    return response.data;
+      
 }
